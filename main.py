70ca1d1ecd15418b2f97abbed068f1150c4e4fe3
Aleksey <aleksey-000@mail.ru>
2026-01-21 23:22:11 +0300

    промежуточный вариант скрипта для формирования отчетов

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging
+from typing import Sequence
 
-from sqlalchemy import select, and_
+from sqlalchemy import select, and_, Row
 from sqlalchemy.orm import Session
 
 from database.db import session_maker
-from database.models import Report
+from database.models import Report, Product
 from s3_storage import storage
 from report_generator import ReportGenerator
 
@@ -20,31 +21,55 @@ class Processor:
                              'группы по типу рк.csv': 'groups',
                              'все кампании.csv': 'campaigns', 'предыдущая рк.csv': 'prev_rk'}
 
-    def get_reports(self):
+    def get_reports(self) -> Sequence[Row[tuple]]:
         """
         Метод получает идентификаторы запросов для формирования отчетов из БД
         :return: список идентификаторов
         """
-        stmt = select(Report.id).where(and_(Report.status_id == 2, Report.to_delete == False))
-        reports_to_process = self.session.execute(stmt).scalars().all()
+        logger.info('Поиск запросов для подготовки отчетов...')
+        stmt = (
+            select(Report.id, Product.name).
+            join(Product, Report.product_id == Product.id).
+            where(and_(Report.status_id == 2, Report.to_delete == False))
+        )
+
+        reports_to_process = self.session.execute(stmt).all()
+        logger.info(f'Найдено {len(reports_to_process)} запросов, готовых к обработке')
         return reports_to_process
 
-    def process_report(self, data):
+    def process_report(self, report_id: int, header: str, outlier_rate: float = 1.5):
+        logger.info(f'Обработка отчета [{report_id}]...')
+        data = self.get_data_content(report_id)
+        if not data:
+            return
+        data['header'] = header
+        data['outlier_rate'] = outlier_rate
+
+        logger.info(f'Формирование файла...')
         new_report = ReportGenerator(**data)
         new_report.write_general_params()
-        new_report.save_report('test.docx')
+        new_report.write_page_views()
+        new_report.write_funnel_graph_section()
+        new_report.write_outliers_section()
+        new_report.write_groups_section()
+        new_report.save_report(f'reports/test{report_id}.docx')
+        logger.info('Файл сформирован')
+        return True
 
-    def get_data_content(self, report_id: int) -> dict:
+    def get_data_content(self, report_id: int) -> dict | None:
         """
         Загрузка данных из S3-хранилища
         :param report_id:
         :return: словарь - имя параметра: csv-данные
         """
-        logger.info('Скачивание данных...')
         path = self.csv_path_template.replace('{{REPORT_ID}}', str(report_id))
+        logger.info(f'Скачивание данных из {path}...')
 
         result = {}
         obj_names = [obj.object_name for obj in storage.get_list_objects(path)]
+        if not obj_names:
+            logger.warning('Нет данных для создания отчета')
+            return None
 
         # поиск и загрузка необходимых файлов бех учета регистра
         for obj_name in obj_names:
@@ -87,9 +112,15 @@ class Processor:
 #         print('спим 60 сек')
 #         time.sleep(60)
 
+corrupted_count = 0
 with session_maker() as session:
     processor = Processor(session)
-    data = processor.get_data_content(101)
-    data['header'] = 'test'
-    data['outlier_rate'] = 1.5
-    processor.process_report(data)
+    # processor.process_report(101, 1.5)
+    reports = processor.get_reports()
+    for report in reports:
+        succues = processor.process_report(report[0], report[1])
+        if not succues:
+            corrupted_count += 1
+        logger.info(f'Обработка отчета [{report[0]}] завершена')
+
+    logger.info(f'{corrupted_count} отчетов не удалось создать')
