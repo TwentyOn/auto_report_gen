f99bb37b77e74b88a5e8b8370e83fa3cfc35a64c
Aleksey <aleksey-000@mail.ru>
2026-01-21 21:35:16 +0300

    добавлена наследуемый аргумент со схемой БД

--- database/db.py
+++ database/db.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-from settings import DB_USER, DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD
+from settings import DB_USER, DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD, DB_SCHEME
 
 DATABASE_URL = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 
 class Base(DeclarativeBase):
-    pass
+    __table_args__ = {'schema': DB_SCHEME}
 
 
 engine = create_engine(DATABASE_URL, echo=True)
