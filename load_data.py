3d3eddab5a00bca9c95d45754a75bbcd7cf60787
Aleksey <aleksey-000@mail.ru>
2026-01-31 21:19:10 +0300

    правка кода по принципам SOLID

--- load_data.py
+++ load_data.py
@@ -1,15 +1,29 @@
 import io
 import os
 
-from docx_report_generator.s3_storage import storage
+from minio import Minio
+import dotenv
 
-FILES_PATH = 'docx_report_generator/example/input_data'
+dotenv.load_dotenv()
 
-for file in os.scandir(FILES_PATH):
-    filename = file.name
+FILES_PATH = 'docx_report_generator/example/input_data'  # путь к csv-файлам
+S3_PATH = 'products_report_generator/{{REPORT_ID}}/csv_exports/{{FILENAME}}'  # шаблон пути для сохранения в S3
 
-    with open(file, encoding='utf-8') as f:
-        file = io.BytesIO(f.read().encode())
-    file.seek(0)
 
-    storage.upload_memory_file(f'products_report_generator/1/csv_exports/{filename}', file, len(file.getvalue()))
+def upload_test_data():
+    access_key = os.getenv('S3_ACCESS_KEY')
+    secret_key = os.getenv('S3_SECRET_KEY')
+    bucket_name = os.getenv('S3_BUCKET_NAME')
+    client = Minio(endpoint='127.0.0.1:9000', access_key=access_key, secret_key=secret_key, secure=False)
+    for file in os.scandir(FILES_PATH):
+        filename = file.name
+
+        with open(file, encoding='utf-8') as f:
+            file = io.BytesIO(f.read().encode())
+        file.seek(0)
+        save_path = S3_PATH.replace('{{REPORT_ID}}', '1').replace('{{FILENAME}}', filename)
+        client.put_object(bucket_name, save_path, file, len(file.getvalue()))
+
+
+if __name__ == '__main__':
+    upload_test_data()
